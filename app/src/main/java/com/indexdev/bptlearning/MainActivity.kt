0ab607aabf0c7fb7166e513be2e7c36b2531cbef
Musnadil <firdaus.musnadil@gmail.com>
2022-11-29 11:56:48 +0700

    setup bottom nav

--- app/src/main/java/com/indexdev/bptlearning/MainActivity.kt
+++ app/src/main/java/com/indexdev/bptlearning/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.indexdev.bptlearning
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.indexdev.bptlearning.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val bottomNav = listOf(
+        R.id.homeFragment,
+        R.id.historyFragment,
+        R.id.profilFragment,
+        R.id.loginFragment
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.fragmentContainer)
+        binding.bottomNavigation.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bottomNavigation.visibility = if (destination.id in bottomNav) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
+        }
     }
 }
