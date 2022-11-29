bf9ac836e9ab972dab35fecea18e9b29319102e3
Musnadil <firdaus.musnadil@gmail.com>
2022-11-29 13:23:01 +0700

    theming

--- app/src/main/java/com/indexdev/bptlearning/ui/home/HomeFragment.kt
+++ app/src/main/java/com/indexdev/bptlearning/ui/home/HomeFragment.kt
@@ -6,12 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.indexdev.bptlearning.R
+import com.indexdev.bptlearning.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val statusBarHeight = resources.getIdentifier("status_bar_height", "dimen", "android")
+        if (statusBarHeight > 0) {
+            binding.statusbar.layoutParams.height = resources.getDimensionPixelSize(statusBarHeight)
+        }
     }
 }
